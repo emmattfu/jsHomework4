2943aac61081442d090f5ecfa5d70b75a420c84f
emmattfu <makcimdjan@gmail.com>
2018-05-18 12:48:17 +0300

    2

--- homework4.js
+++ homework4.js
@@ -13,6 +13,7 @@ function multiply() {
 
 function reverseString(string) {
     if (typeof string !== 'string') return new Error('Not a string');
+    if (!text.length) return new Error('text empty');
     let str = '';
     for (let i = 1; i <= string.length; i++) {
         str += string[string.length - i];
@@ -21,11 +22,58 @@ function reverseString(string) {
 }
 
 // Задача № 4
-
 function getCodeStringFromText(string) {
     let stringFromText = '';
     for (let i = 0; i < string.length; i++) {
         stringFromText += string.charCodeAt(i) + ' ';
     }
     return stringFromText;
+}
+
+
+// Todo
+// Add, Edit, Delete
+let todos = [
+    {
+        text: 'Learn JavaScript',
+        id: 0
+    },
+    {
+        text: 'Learn Angular',
+        id: 1
+    }
+];
+
+function addTask(text) {
+    if (typeof text !== 'string') return new Error('text is not a string');
+    if (!text.length) return new Error('text empty');
+
+    // Create new task obj
+    const newTask = {
+        id: todos.length,
+        text
+    };
+
+    // Add new task
+    todos.push(newTask); // push, pop, unshift, shift
+
+    return todos;
+}
+
+function deleteTask(id) {
+    if (id !== 0 && !id) return new Error('id required');
+    if (isNaN(id)) return new Error('id must be a number');
+    id = Number(id);
+
+    let todo;
+
+    for (let i = 0; i < todos.length; i++ ) {
+        if (todos[i].id === id) {
+            todo = todos[i];
+            todos.splice(i, 1);
+            break;
+        }
+    }
+
+    return todo || 'task not found';
 }
